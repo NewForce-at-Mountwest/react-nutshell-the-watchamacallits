de6a66967fba6e79ea45a467af42662f060a820c
Devin Conroy <d.conroy1991@gmail.com>
2020-06-08 14:59:51 -0400

    first push for team

---
+++ react-nutshell/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Nutshell from './components/Nutshell';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Nutshell />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
